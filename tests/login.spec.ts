e47ee47f1e3d8822d51a5c58e11775a94f238468
cernestomtz <c.ernesto.mtz@gmail.com>
2026-06-19 14:20:07 -0600

    Force failing test to validate branch protection

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,7 +10,8 @@ test('TC-001 Valid Login - User can access inventory page', async ({ page }) => 
   await loginPage.login(users.standard.username, users.standard.password);
 
   await expect(page).toHaveURL(/.*inventory.html/);
-  await expect(page.getByText('Products')).toBeVisible();
+  //await expect(page.getByText('Products')).toBeVisible();
+  await expect(page.getByText('FAIL')).toBeVisible();
 });
 
 test('TC-002 Invalid Login - User cannot login with invalid credentials', async ({ page }) => {
