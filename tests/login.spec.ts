3a58a841bb6897ef892fb685e4a3b7d837185ff3
cernestomtz <c.ernesto.mtz@gmail.com>
2026-06-22 09:02:53 -0600

    correct

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,8 +13,9 @@ test('TC-001 Valid Login - User can access inventory page', async ({ page }) => 
   await loginPage.login(users.standard.username, users.standard.password);
 
   await expect(page).toHaveURL(/.*inventory.html/);
-  //await expect(page.getByText('Products')).toBeVisible();
-  await expect(page.getByText('FAIL')).toBeVisible();
+  await expect(page.getByText('Products')).toBeVisible();
+  //this will not work
+  //await expect(page.getByText('FAIL')).toBeVisible();
 });
 
 test('TC-002 Invalid Login - User cannot login with invalid credentials', async ({ page }) => {
